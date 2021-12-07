3a5a487432a9e42453e547cc334ce0871b22e83d
Megaqualle <thore@witting.biz>
2021-12-07 23:31:48 +0100

    init

---
+++ BaseWorld.java
@@ -0,0 +1,23 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.List;
+import javax.swing.JOptionPane;
+
+/**
+ * Write a description of class MyWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class BaseWorld extends World
+{
+
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
+    public BaseWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(970, 540, 1);
+    }
+}
